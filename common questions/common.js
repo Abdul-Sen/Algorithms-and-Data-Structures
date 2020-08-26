79749ecfc74050f1b4f39c884c35add7642adeba
Abdul <abdul.rm.jahangir@gmail.com>
2020-08-26 17:48:13 -0400

    added quicksort (recursive)

--- common questions/common.js
+++ common questions/common.js
@@ -1,14 +1,11 @@
 let result = null;
 
 // Q - Is string a palindrome
-function isPalindrome(word)
-{
+function isPalindrome(word) {
     word = [...word];
 
-    for(let i = 0; i < word.length / 2 ; i++)
-    {
-        if(word[i] !== word[word.length -1  - i])
-        {
+    for (let i = 0; i < word.length / 2; i++) {
+        if (word[i] !== word[word.length - 1 - i]) {
             return false;
         }
     }
@@ -22,26 +19,22 @@ function isPalindrome(word)
  * @param {Number} val - the value we want to search.
  * @param {Number} prefix - Carries the prefixes from previous arrays (since we are cutting down array) to be added to the final index.
  */
-function binarySearch(sortedArray,val, prefix = 0)
-{
-    let midIndex = Math.floor((sortedArray.length/ 2));
+function binarySearchRecursive(sortedArray, val, prefix = 0) {
+    let midIndex = Math.floor((sortedArray.length / 2));
     let newArray = null;
 
-    if(sortedArray[midIndex] === val)
-    {
+    if (sortedArray[midIndex] === val) {
         return prefix + midIndex;
     }
-    else if(val < sortedArray[midIndex])
-    {
+    else if (val < sortedArray[midIndex]) {
         newArray = sortedArray.slice(0, midIndex);
     }
-    else if(val > sortedArray[midIndex])
-    {
+    else if (val > sortedArray[midIndex]) {
         newArray = sortedArray.slice(midIndex);
         prefix += midIndex;
     }
 
-    return binarySearch(newArray,val, prefix);
+    return binarySearch(newArray, val, prefix);
 }
 
 
@@ -50,34 +43,59 @@ function binarySearch(sortedArray,val, prefix = 0)
  * @param {array} array - the sorted array to search through
  * @param {Number} val - the item/number we want to search.
  */
-function nonRecursiveBinarySearch(array, val)
-{
+function BinarySearchIterative(array, val) {
     let min = 0;
     let max = array.length - 1;
     let guessIndex = null;
-    
-    while(min < max)
-    {
-        guessIndex = Math.floor((min + max)/2);
 
-        if(array[guessIndex] === val)
-        {
+    while (min < max) {
+        guessIndex = Math.floor((min + max) / 2);
+
+        if (array[guessIndex] === val) {
             return guessIndex;
         }
 
-        if(array[guessIndex] < val)
-        {
+        if (array[guessIndex] < val) {
             min = guessIndex + 1;
         }
-        if(array[guessIndex] > val)
-        {
+        if (array[guessIndex] > val) {
             max = guessIndex - 1;
         }
     }
     return -1;
 }
 
-// TODO quicksort
+/**
+ * recursive implimentation of quicksort using middle pivot.
+ * @param {array} array - the unsorted array
+ */
+function quicksortRecursive(array) {
+
+    // base condition
+    if (array.length <= 1) {
+        return array;
+    }
+
+    let pivot = array[0];
+
+    let left = [];
+    let right = [];
+
+    for (let i = 1; i < array.length; i++) {
+        if (array[i] < pivot) {
+            left.push(array[i]);
+        }
+        else {
+            right.push(array[i]);
+        }
+    }
+
+    left = quicksort(left);
+    right = quicksort(right);
+
+    return [...left, pivot, ...right]; // joniing left, middle and right array
+};
+
 
 // TODO recursive fibinnaci
 
